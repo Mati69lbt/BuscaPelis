f5c2d93e5fd6d1e2eb6a170626ee9289f673af73
Mati69lbt <Mati69_lbt@hotmail.com>
2023-06-01 14:23:26 -0300

    Botones

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -24,23 +24,37 @@ const Home = () => {
       <Navbar />
 
       <div className="cuerpo">
+        {/* desde haca */}
         <div className="buscador">
-          <h2 className="busch2">Buscador: </h2>
-          <form onSubmit={(e) => handleSubmit(e)}>
-            <input
-              id="title"
-              autoComplete="off"
-              value={title}
-              onChange={(e) => handleChange(e)}
-              type="search"
-              placeholder=" Search"
-              aria-label="Search"
-            />
-            <button type="submit" onClick={() => dispatch(getMovies(title))}>
-              Buscar
-            </button>
-          </form>
+          <div className="a">
+            <form onSubmit={(e) => handleSubmit(e)}>
+              <div className="input-container">
+                <input
+                  id="title"
+                  autoComplete="off"
+                  value={title}
+                  onChange={(e) => handleChange(e)}
+                  type="search"
+                  placeholder="Search"
+                  aria-label="Search"
+                />
+
+                <button
+                  className="Btn"
+                  onClick={() => dispatch(getMovies(title))}
+                >
+                  <svg viewBox="0 0 512 512" className="svgIcon" height="1em">
+                    <path d="M288 448H64V224h64V160H64c-35.3 0-64 28.7-64 64V448c0 35.3 28.7 64 64 64H288c35.3 0 64-28.7 64-64V384H288v64zm-64-96H448c35.3 0 64-28.7 64-64V64c0-35.3-28.7-64-64-64H224c-35.3 0-64 28.7-64 64V288c0 35.3 28.7 64 64 64z"></path>
+                  </svg>
+                  <p className="text">Buscar</p>
+                  <span className="effect"></span>
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
+
+        {/* hasta aca!!  */}
         <div>
           {!movies || movies.length === 0 ? (
             <h1 className="bup">
--- src/components/Nav/Navbar.jsx
+++ src/components/Nav/Navbar.jsx
@@ -7,9 +7,22 @@ const Navbar = () => {
   return (
     <div className="nav">
       <div className="navbar">
-        <button onClick={() => navigate("/home")}>HOME</button>
+        <button onClick={() => navigate("/home")} className="Btn">
+          <svg viewBox="0 0 512 512" className="svgIcon" height="1em">
+            <path d="M288 448H64V224h64V160H64c-35.3 0-64 28.7-64 64V448c0 35.3 28.7 64 64 64H288c35.3 0 64-28.7 64-64V384H288v64zm-64-96H448c35.3 0 64-28.7 64-64V64c0-35.3-28.7-64-64-64H224c-35.3 0-64 28.7-64 64V288c0 35.3 28.7 64 64 64z"></path>
+          </svg>
+          <p className="text">HOME</p>
+          <span className="effect"></span>
+        </button>
+
         <h1 className="title">BUSCADOR DE PELICULAS</h1>
-        <button onClick={() => navigate("/favoritas")}>FAV</button>
+        <button onClick={() => navigate("/favoritas")} className="Btn">
+          <svg viewBox="0 0 512 512" className="svgIcon" height="1em">
+            <path d="M288 448H64V224h64V160H64c-35.3 0-64 28.7-64 64V448c0 35.3 28.7 64 64 64H288c35.3 0 64-28.7 64-64V384H288v64zm-64-96H448c35.3 0 64-28.7 64-64V64c0-35.3-28.7-64-64-64H224c-35.3 0-64 28.7-64 64V288c0 35.3 28.7 64 64 64z"></path>
+          </svg>
+          <p className="text">FAV</p>
+          <span className="effect"></span>
+        </button>
       </div>
       <hr />
     </div>
